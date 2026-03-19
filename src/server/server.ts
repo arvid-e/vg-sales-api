07095c1863b4318ab97cefb25ac81780ba8dd8dc
arvid-e <arvid.edvinsson@gmail.com>
2026-03-19 12:52:24 +0900

    Update port fallback

--- src/server/server.ts
+++ src/server/server.ts
@@ -12,9 +12,13 @@ let server: Server | undefined;
 export const startServer = async () => {
   try {
     await connectDB();
-    server = app.listen(port, () => {
-      console.log(`Server running at port: ${port}`);
+    
+    const PORT = process.env.PORT || 3000;
+
+    server = app.listen(Number(PORT), '0.0.0.0', () => {
+      console.log(`Server running at port: ${PORT}`);
     });
+    
   } catch (error: unknown) {
     if (isErrorWithMessage(error)) {
       console.error('Failed to start server:', error.message);
