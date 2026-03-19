b5bc38db70505ae61ce36d5a96bd1eb2ef343256
arvid-e <arvid.edvinsson@gmail.com>
2026-03-19 17:27:33 +0900

    Restrict char lenght in username and password

--- src/server/models/UserModel.ts
+++ src/server/models/UserModel.ts
@@ -6,10 +6,13 @@ const userSchema = new Schema<IUserDocument>(
     username: {
       type: String,
       required: true,
+      unique: true,
+      minlength: [3, 'Username must be at least 3 characters'],
     },
     password: {
       type: String,
       required: true,
+      minlength: [10, 'Password must be at least 10 characters'],
       select: false,
     },
   },
